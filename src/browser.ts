d91feb7f672b896111ec835a80f053bb795ab974
Daniel Worthington-Bodart <dan@bodar.com>
2018-02-12 17:09:46 +0000

    WIP

--- src/browser.ts
+++ src/browser.ts
@@ -1,9 +1,20 @@
 import {HttpHandler, Request, Response} from "./api";
 
-export class ClientHttpHandler implements HttpHandler{
+export class ClientHttpHandler implements HttpHandler {
     handle(request: Request): Promise<Response> {
         return new Promise<Response>(() => {
 
         });
     }
+}
+
+export class HelloWorld implements HttpHandler {
+    handle({method, url}: Request): Promise<Response> {
+        return new Promise<Response>(() => {
+            switch(method){
+                case 'GET': return Response
+            }
+        });
+    }
+
 }
