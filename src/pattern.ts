f5544b9c5736940f00515e5397cfa576d0a5cc9a
Daniel Worthington-Bodart <dan@bodar.com>
2018-02-15 21:47:08 +0000

    Got first version of sub pattern match working

--- src/pattern.ts
+++ src/pattern.ts
@@ -1,38 +1,49 @@
-export interface Matcher<T, R> {
-    matches(instance: T): R | undefined
-}
-
-export class CaseMatcher<T, R> implements Matcher<T, R> {
-    constructor(private filter: Partial<T>, private handler: (instance: T) => R) {
-    }
-
-    matches(instance: T): R | undefined {
-        if (isPartial(instance, this.filter)) {
-            return this.handler(instance);
-        }
-        return undefined;
-    }
-}
-
-export function case_<T, R>(partial: Partial<T>, handler: (instance: T) => R): Matcher<T, R> {
-    return new CaseMatcher(partial, handler)
-}
-
-export function match<T, R>(instance: T, ...patterns: Matcher<T, R>[]): R {
-    for (let i = 0; i < patterns.length; i++) {
-        const pattern = patterns[i];
-        const result = pattern.matches(instance);
+export function match<T, R>(instance: T, ...matchers: Matcher<T, R>[]): R {
+    for (let i = 0; i < matchers.length; i++) {
+        const matcher = matchers[i];
+        const result = matcher.matches(instance);
         if (typeof result != 'undefined') return result;
     }
     throw new Error("Non exhaustive matches detected");
 }
 
+export interface Matcher<T, R> {
+    matches(instance: T): R | undefined
+}
 
-export function isPartial<T>(instance: T, partial: Partial<T>): boolean {
-    if (!partial) {
-        return true;
+export function case_<T, R>(pattern: Pattern<T>, handler: (instance: PatternResult<T>) => R): Matcher<T, R> {
+    return new CaseMatcher(pattern, handler)
+}
+
+export class CaseMatcher<T, R> implements Matcher<T, R> {
+    constructor(private pattern: Pattern<T>, private handler: (instance: PatternResult<T>) => R) {
     }
 
+    matches(instance: T): R | undefined {
+        let result = apply(instance, this.pattern);
+        if (typeof result == 'undefined') return undefined;
+        return this.handler(result);
+    }
+}
+
+export type Pattern<T> = { [P in keyof T]?: T[P] | Matcher<T[P], {} | any[]>; }
+export type PatternResult<T> = { [P in keyof T]?: T[P] | {} | any[]; }
+
+export function regex(value: RegExp): RegexMatcher {
+    return new RegexMatcher(value)
+}
+
+export class RegexMatcher implements Matcher<string, string[]> {
+    constructor(private value: RegExp) {
+    }
+
+    matches(instance: string): string[] | undefined {
+        const match = this.value.exec(instance);
+        return match ? match : undefined;
+    }
+}
+
+export function isPartial<T>(instance: T, partial: Partial<T>): boolean {
     return Object.keys(partial).every(function (k) {
         let key = k as keyof T;
         let actual: any = instance[key];
@@ -43,4 +54,27 @@ export function isPartial<T>(instance: T, partial: Partial<T>): boolean {
         }
         return partial[key] === actual;
     });
+}
+
+export function apply<T>(instance: T, pattern: Pattern<T>): PatternResult<T> | undefined {
+    let clone: PatternResult<T> = Object.assign({}, instance);
+    const keys = Object.keys(pattern);
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i] as keyof T;
+        let actual: any = instance[key];
+        let expected: any = pattern[key];
+
+        if (typeof actual == 'string' && expected instanceof Object && 'matches' in expected) {
+            let result = expected.matches(actual);
+            if (typeof result == undefined) return undefined;
+            if (Array.isArray(result)) clone[key] = result.slice();
+            clone[key] = result;
+        } else if (actual instanceof Object && expected instanceof Object) {
+            let result = apply(actual, expected);
+            if (typeof result == undefined) return undefined;
+            clone[key] = result;
+        } else if (pattern[key] !== actual) return undefined;
+    }
+
+    return clone;
 }
--- test/pattern.test.ts
+++ test/pattern.test.ts
@@ -1,6 +1,6 @@
 import {assert} from 'chai';
 import {get, post, Request} from "../src/api";
-import {match, case_, isPartial} from "../src/pattern";
+import {match, case_, isPartial, Pattern, regex, apply} from "../src/pattern";
 
 describe('pattern matching', function () {
     it('can verify a partial objects values match', function () {
@@ -11,16 +11,44 @@ describe('pattern matching', function () {
 
     it('can match against a partial instance', function () {
         assert(match(get('/some/path'),
-            case_({uri: '/some/path'} as Partial<Request>, (request) => request.method)) === "GET");
+            case_({uri: '/some/path'} as Partial<Request>, (request) => request.method)) == "GET");
         assert(match(post('/some/path'),
-            case_({uri: '/some/path'} as Partial<Request>, (request) => request.method)) === "POST");
-
+            case_({uri: '/some/path'} as Partial<Request>, (request) => request.method)) == "POST");
     });
 
     it('can destructure what was matched', function () {
         assert(match(get('/some/path'),
-            case_({uri: '/some/path'} as Partial<Request>, ({method}) => method)) === "GET");
+            case_({uri: '/some/path'} as Partial<Request>, ({method}) => method)) == "GET");
     });
+
+    it('pattern instance must match keys and values', function () {
+        const pattern: Pattern<Request> = {uri: '/some/path'}
+    });
+
+    it('pattern instance values can be RegExp', function () {
+        const pattern: Pattern<Request> = {uri: regex(/foo/)}
+    });
+
+    it('regex returns capture groups', function () {
+        let groups = regex(/Hello (World)/).matches('Hello World');
+        if (groups == undefined) throw new Error();
+        assert(groups[0] == 'Hello World');
+        assert(groups[1] == 'World');
+    });
+
+    it('pattern instance mixes in result', function () {
+        const pattern: Pattern<Request> = {uri: regex(/Hello (World)/)};
+        const request = get('Hello World');
+        const mixed: any = apply(request, pattern);
+        assert(mixed.method == 'GET');
+        assert(mixed.uri[1] == 'World');
+    });
+
+    it('can match against a pattern instance', function () {
+        assert(match(get('Hello World'),
+            case_({uri: regex(/Hello (World)/)} as Pattern<Request>, ({uri:[, w]}) => w)) == "World");
+    });
+
 });
 
 
