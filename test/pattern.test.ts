2cc59614537be63880eddce96d5bfbcfb1338cb3
Daniel Worthington-Bodart <dan@bodar.com>
2018-02-15 07:26:40 +0000

    Improved tests

--- test/pattern.test.ts
+++ test/pattern.test.ts
@@ -5,6 +5,8 @@ import {match, case_, isPartial} from "../src/pattern";
 describe('pattern matching', function () {
     it('can verify a partial of an object at runtime', function () {
         assert(isPartial(get('/some/path'), {method: 'GET'}));
+        assert(isPartial(get('/some/path'), {uri: '/some/path'}));
+        assert(!isPartial(get('/some/path'), {method: 'POST'}));
     });
 
     it('can match against a partial instance', function () {
