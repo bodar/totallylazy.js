7a0f47a513b2e3e5ea2ff8d08cf9d9fe32e3749a
Daniel Worthington-Bodart <dan@bodar.com>
2018-02-20 17:15:01 +0000

    Made async

--- src/browser/browser.test.ts
+++ src/browser/browser.test.ts
@@ -4,6 +4,7 @@ import {handlerContract} from "../handler.contract";
 describe("XmlHttpHandler", function () {
     handlerContract(async () => {
         if (typeof XMLHttpRequest == 'undefined') return Promise.reject("");
+
         const {XmlHttpHandler} = await import('./browser');
         return new XmlHttpHandler();
     });
--- src/node/server.ts
+++ src/node/server.ts
@@ -3,7 +3,7 @@ import {request, IncomingMessage} from 'http';
 import {URL} from 'url';
 
 export class NodeClientHandler implements Handler {
-    handle(req: Request): Promise<Response> {
+    async handle(req: Request): Promise<Response> {
         return new Promise<Response>((resolve, reject) => {
                 const url = new URL(req.uri);
                 request({method: req.method, path: url.pathname, hostname: url.host}, (res: IncomingMessage) => {
