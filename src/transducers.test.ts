d5cb06fbf750f35a4cb8e1ccd5777ec663f09a5c
Daniel Worthington-Bodart <dan@bodar.com>
2018-03-09 07:27:32 +0000

    Minor clean up

--- src/transducers.test.ts
+++ src/transducers.test.ts
@@ -1,26 +1,21 @@
 import {assert} from 'chai';
-import {identity, sum, toArray} from "./transducers";
+import {transducer, sum, toArray} from "./transducers";
 
 
 describe("transducers", () => {
     it("can map", async () => {
-        const transducer = identity<number>().map(n => n.toString());
-        return assertResult(transducer.call(2), "2");
+        return assertResult(transducer<number>().map(n => n.toString()).call(2), "2");
     });
 
     it("can filter", async () => {
-        const transducer = identity<number>().filter(n => n % 2 == 0);
-        return assertResult(transducer.call(0, 1, 2, 3, 4), 0, 2, 4);
+        return assertResult(transducer<number>().filter(n => n % 2 == 0).call(0, 1, 2, 3, 4), 0, 2, 4);
     });
 
     it("can scan", async () => {
-        const transducer = identity<number>().scan(sum);
-        return assertResult(transducer.call(0, 2, 4), 0, 2, 6);
+        return assertResult(transducer<number>().scan(sum).call(0, 2, 4), 0, 2, 6);
     });
 
     async function assertResult<T>(iterable: AsyncIterable<T>, ...expected: T[]) {
         assert.deepEqual(await toArray(iterable), expected);
     }
-
-
 });
