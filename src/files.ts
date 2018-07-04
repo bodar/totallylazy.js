85f21d9b25d5aaa3596f77d346607342da676690
Daniel Worthington-Bodart <dan@bodar.com>
2018-07-04 16:30:27 +0100

    Added simple File.stream methods

--- src/files.ts
+++ src/files.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import {promisify} from 'util';
 import {lazy} from './lazy';
 import {Stats} from "fs";
+import {Readable, Writable} from 'stream';
 
 if (typeof Symbol.asyncIterator == 'undefined') {
     (Symbol as any).asyncIterator = Symbol.for("Symbol.asyncIterator");
@@ -21,11 +22,11 @@ export class File {
         return `file://${this.absolutePath}`;
     }
 
-    child(name:string){
+    child(name: string) {
         return new File(name, this.absolutePath);
     }
 
-    async * children(): AsyncIterable<File> {
+    async* children(): AsyncIterable<File> {
         const names: string[] = await promisify(fs.readdir)(this.absolutePath);
         yield* names.map(name => this.child(name));
     }
@@ -42,7 +43,7 @@ export class File {
         return promisify(fs.lstat)(this.absolutePath);
     }
 
-    async * descendants(): AsyncIterable<File> {
+    async* descendants(): AsyncIterable<File> {
         for await (const child of this.children()) {
             if (await child.isDirectory) yield* child.descendants();
             yield child;
@@ -57,12 +58,21 @@ export class File {
         return (await promisify(fs.readFile)(this.absolutePath, 'utf-8')).toString();
     }
 
+    read(options?: StreamOptions): Readable {
+        return fs.createReadStream(this.absolutePath, options);
+    }
+
     async append(data: any, options?: FileOptions): Promise<void> {
         return await promisify(fs.appendFile)(this.absolutePath, data, options)
     }
 
-    async mkdir(): Promise<File>{
-        if(!await this.exists) await promisify(fs.mkdir)(this.absolutePath);
+    write(options?: StreamOptions): Writable {
+        return fs.createWriteStream(this.absolutePath, options);
+    }
+
+
+    async mkdir(): Promise<File> {
+        if (!await this.exists) await promisify(fs.mkdir)(this.absolutePath);
         return this;
     }
 
@@ -77,11 +87,11 @@ export class File {
 
     async copy(destination: string | File, flags?: number): Promise<void> {
         destination = destination instanceof File ? destination : new File(destination);
-        if(await this.isDirectory){
+        if (await this.isDirectory) {
             const dest = await destination.child(this.name).mkdir();
             for await (const descendant of this.descendants()) await descendant.copy(dest, flags);
         } else {
-            if(await destination.isDirectory) destination = destination.child(this.name);
+            if (await destination.isDirectory) destination = destination.child(this.name);
             return await promisify(fs.copyFile)(this.absolutePath, destination.absolutePath, flags);
         }
     }
@@ -89,4 +99,15 @@ export class File {
 }
 
 export type FileOptions = { encoding?: string | null, mode?: string | number, flag?: string } | string
+
+export type StreamOptions = string | {
+    flags?: string;
+    encoding?: string;
+    fd?: number;
+    mode?: number;
+    autoClose?: boolean;
+    start?: number;
+    end?: number;
+    highWaterMark?: number;
+}
 
