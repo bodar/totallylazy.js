d7fddf8f547567e4ef812246415a1fcc557442f4
Daniel Worthington-Bodart <dan@bodar.com>
2018-11-02 11:43:36 +0000

    added simple container, thanks Matt

--- src/lazy.ts
+++ src/lazy.ts
@@ -5,3 +5,14 @@ export function replace<T extends {}, K extends keyof T>(object: T, key: K, valu
 export function lazy<T extends {}, K extends keyof T>(object: T, key: K, value: T[K]): T[K] {
     return replace(object, key, value)[key];
 }
+
+export function container<C>(activators: Activators<C>): C {
+    return Object.keys(activators).reduce((container, activator) =>
+        Object.defineProperty(container, activator, {
+            configurable: true,
+            get: () => lazy(container, activator as keyof C, (activators as any)[activator](container))
+        }), <C>{});
+}
+
+export type Activator<C, T> = (c: C) => T;
+export type Activators<C> = { [P in keyof C]: Activator<C, C[P]> }
