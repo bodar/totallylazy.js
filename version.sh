57e48826e282ee9d9805d7753d98856aa699ba35
Daniel Worthington-Bodart <dan@bodar.com>
2018-12-19 22:29:23 +0000

    Switched version to use circleci parameters

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export BRANCH=${BRANCH_NAME-$(git rev-parse --abbrev-ref HEAD)}
-export BUILD_NUMBER=${BUILD_NUMBER-$(date +"%Y%m%d%H%M%S" -u)+${USER}}
+export BRANCH=${CIRCLE_BRANCH-$(git rev-parse --abbrev-ref HEAD)}
+export BUILD_NUMBER=${CIRCLE_BUILD_NUM-$(date +"%Y%m%d%H%M%S" -u)}
 
 if [ "${CI}" = "true" ] && [ "${BRANCH}" = "master" ]; then
     export TAG="latest"
