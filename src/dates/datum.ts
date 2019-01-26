ef5fd6456bb7fd843f228dfc7da42f76a020a748
Daniel Worthington-Bodart <dan@bodar.com>
2019-01-26 21:34:06 +0000

    Dates: added cache for data extractors

--- src/dates/datum.ts
+++ src/dates/datum.ts
@@ -31,12 +31,12 @@ export class DatumLookup<T extends Datum = Datum> {
 
     get(number: number): T {
         const result = this.data[0][number - 1];
-        if(!result) throw new Error(`${this.constructor.name} - Illegal argument: number was out of range : ${number}`);
+        if (!result) throw new Error(`${this.constructor.name} - Illegal argument: number was out of range : ${number}`);
         return result;
     }
 
     get pattern(): string {
-        const min = flatten(this.data).reduce((a,l) => Math.min(a, l.name.length), Number.MAX_VALUE);
+        const min = flatten(this.data).reduce((a, l) => Math.min(a, l.name.length), Number.MAX_VALUE);
         return `[${this.characters.join('')}]{${min},${this.prefixTree.height}}`;
     }
 
@@ -47,7 +47,7 @@ export class DatumLookup<T extends Datum = Datum> {
 
 export type Month = Datum;
 
-export class Months extends DatumLookup<Month>{
+export class Months extends DatumLookup<Month> {
     static formats: Options[] = [
         {month: "long"}, {month: "short"},
         {year: 'numeric', month: "long", day: 'numeric'},
@@ -72,13 +72,13 @@ export class Months extends DatumLookup<Month>{
         return Months.formats.map(f => Months.dataFor(locale, f));
     }
 
-    static dataFor(locale: string, options:Options): Month[] {
+    static dataFor(locale: string, options: Options): Month[] {
         return months(locale, options, hasNativeFormatToParts).map((m, i) => ({name: m, number: i + 1}));
     }
 }
 
 // No dep version
-function range(start:number, end:number):number[]{
+function range(start: number, end: number): number[] {
     const result = [];
     for (let i = start; i <= end; i++) {
         result.push(i);
@@ -86,19 +86,24 @@ function range(start:number, end:number):number[]{
     return result;
 }
 
+const months_cache: { [key: string]: string[] } = {};
+
 export function months(locale: string = 'default', monthFormat: MonthFormat | Options = 'long', native = hasNativeFormatToParts): string[] {
-    const options: Options = {...typeof monthFormat == 'string' ? {month: monthFormat} : monthFormat};
+    const key = JSON.stringify({locale, monthFormat, native});
+    return months_cache[key] = months_cache[key] || (() => {
+        const options: Options = {...typeof monthFormat == 'string' ? {month: monthFormat} : monthFormat};
 
-    const dates = range(1,12).map(i => date(2000, i, 1));
+        const dates = range(1, 12).map(i => date(2000, i, 1));
 
-    if(native) return new NativeDataExtractor(locale, options, dates, 'month').extract();
-    return new FromFormatStringMonthExtractor(locale, options, dates).extract();
+        if (native) return new NativeDataExtractor(locale, options, dates, 'month').extract();
+        return new FromFormatStringMonthExtractor(locale, options, dates).extract();
+    })();
 }
 
 
 export type Weekday = Datum;
 
-export class Weekdays extends DatumLookup<Weekday>{
+export class Weekdays extends DatumLookup<Weekday> {
     static formats: Options[] = [
         {weekday: "long"}, {weekday: "short"},
         {year: 'numeric', month: "numeric", weekday: 'long'},
@@ -122,23 +127,26 @@ export class Weekdays extends DatumLookup<Weekday>{
         return Weekdays.formats.map(f => Weekdays.dataFor(locale, f));
     }
 
-    static dataFor(locale: string, options:Options): Weekday[] {
+    static dataFor(locale: string, options: Options): Weekday[] {
         return weekdays(locale, options).map((m, i) => ({name: m, number: i + 1}));
     }
 }
 
+const weekdays_cache: { [key: string]: string[] } = {};
 
+export function weekdays(locale: string = 'default', weekdayFormat: WeekdayFormat | Options = 'long', native = hasNativeFormatToParts): string[] {
+    const key = JSON.stringify({locale, weekdayFormat, native});
+    return weekdays_cache[key] = weekdays_cache[key] || (() => {
+        const options: Options = {...typeof weekdayFormat == 'string' ? {weekday: weekdayFormat} : weekdayFormat};
 
-export function weekdays(locale: string = 'default', weekdayFormat: WeekdayFormat | Options = 'long', native=hasNativeFormatToParts): string[] {
-    const options: Options = {...typeof weekdayFormat == 'string' ? {weekday: weekdayFormat} : weekdayFormat};
+        const dates = range(1, 7).map(i => date(2000, 1, i + 2));
 
-    const dates = range(1,7).map(i => date(2000, 1, i + 2));
-
-    if(native) return new NativeDataExtractor(locale, options, dates, 'weekday').extract();
-    return new FromFormatStringWeekdayExtractor(locale, options, dates).extract();
+        if (native) return new NativeDataExtractor(locale, options, dates, 'weekday').extract();
+        return new FromFormatStringWeekdayExtractor(locale, options, dates).extract();
+    })();
 }
 
-export function exactFormat(locale: string, options:Options,  dates:Date[]): string[] {
+export function exactFormat(locale: string, options: Options, dates: Date[]): string[] {
     const formatter = Formatters.create(locale, options);
     return dates.map(d => formatter.format(d));
 }
@@ -147,11 +155,11 @@ export interface DataExtractor {
     extract(): string[];
 }
 
-export class BaseDataExtractor{
-    constructor(protected locale:string,
-                protected options:Options,
-                protected dates:Date[],
-                protected partType:DateTimeFormatPartTypes){
+export class BaseDataExtractor {
+    constructor(protected locale: string,
+                protected options: Options,
+                protected dates: Date[],
+                protected partType: DateTimeFormatPartTypes) {
     }
 }
 
@@ -166,7 +174,7 @@ export abstract class FromFormatStringDataExtractor extends BaseDataExtractor im
     extract(): string[] {
         const exact = Object.keys(this.options).length == 1;
         const fullFormats = exactFormat(this.locale, this.options, this.dates);
-        if(exact) return fullFormats;
+        if (exact) return fullFormats;
         const simpleFormats = exactFormat(this.locale, {[this.partType]: (this.options as any)[this.partType]}, this.dates);
         const diffs = this.diff(fullFormats);
         const result = [];
@@ -180,19 +188,19 @@ export abstract class FromFormatStringDataExtractor extends BaseDataExtractor im
         return result;
     }
 
-    diff(data:string[]):string[] {
+    diff(data: string[]): string[] {
         return different(data);
     }
 }
 
-export class FromFormatStringMonthExtractor extends FromFormatStringDataExtractor{
+export class FromFormatStringMonthExtractor extends FromFormatStringDataExtractor {
     constructor(locale: string, options: Options, dates: Date[]) {
         super(locale, options, dates, 'month');
     }
 
-    diff(data:string[]):string[] {
-        if(!this.options.weekday) return super.diff(data);
-        const result:string[] = [];
+    diff(data: string[]): string[] {
+        if (!this.options.weekday) return super.diff(data);
+        const result: string[] = [];
         const days = weekdays(this.locale, this.options);
         const weekday = days[this.day(this.dates[8])];
         for (let i = 0; i < data.length; i++) {
@@ -207,19 +215,19 @@ export class FromFormatStringMonthExtractor extends FromFormatStringDataExtracto
 
     private day(date: Date) {
         const day = date.getDay();
-        if(day == 0) return 6;
+        if (day == 0) return 6;
         return day - 1;
     }
 }
 
-export class FromFormatStringWeekdayExtractor extends FromFormatStringDataExtractor{
+export class FromFormatStringWeekdayExtractor extends FromFormatStringDataExtractor {
     constructor(locale: string, options: Options, dates: Date[]) {
         super(locale, options, dates, 'weekday');
     }
 
-    diff(data:string[]):string[] {
-        if(!this.options.day) return super.diff(data);
-        const result:string[] = [];
+    diff(data: string[]): string[] {
+        if (!this.options.day) return super.diff(data);
+        const result: string[] = [];
         const day = this.dates[0].getDate().toString();
         for (let i = 0; i < data.length; i++) {
             const f = data[i];
