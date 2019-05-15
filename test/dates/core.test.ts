6cefda03f32c9dd9679ba40c2d34b0a5d0fb6834
Matt Green <mjgreen145@gmail.com>
2019-05-15 15:31:22 +0100

    Contruct dates using UTC parameters

--- test/dates/core.test.ts
+++ test/dates/core.test.ts
@@ -6,4 +6,9 @@ describe("core", function () {
         assert.equal(date(2000, 1, 2).toISOString(), '2000-01-02T00:00:00.000Z');
         assert.equal(date(2001, 2, 28).toISOString(), '2001-02-28T00:00:00.000Z');
     });
+
+    it('defaults to 1st January if no month or date are provided', () => {
+        assert.equal(date(2000).toISOString(), '2000-01-01T00:00:00.000Z');
+        assert.equal(date(2000, 2).toISOString(), '2000-02-01T00:00:00.000Z');
+    });
 });
