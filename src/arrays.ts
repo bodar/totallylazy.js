58af71cba3a106e48d75506df755acee45eee943
Daniel Worthington-Bodart <dan@bodar.com>
2019-05-25 06:04:23 +0100

    ie11: fix for bad Set constructor

--- src/arrays.ts
+++ src/arrays.ts
@@ -2,8 +2,10 @@ export function flatten<T>(values: T[][]): T[] {
     return values.reduce((a, ms) => a.concat(ms), []);
 }
 
+const setSupported = typeof Set === 'function' && new Set([1]).size === 1;
+
 export function unique<T>(a: T[]): T[] {
-    if (typeof Array.from === 'function' && typeof Set === 'function') return Array.from(new Set(a));
+    if (typeof Array.from === 'function' && setSupported) return Array.from(new Set(a));
 
     const result = [];
     for (const item of a) {
