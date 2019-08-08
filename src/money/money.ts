e8be3b208f89bc3dd4c91b28ba52b6110b9e6513
Daniel Worthington-Bodart <dan@bodar.com>
2019-08-08 15:02:42 +0100

    money: added basic symbol support

--- src/money/money.ts
+++ src/money/money.ts
@@ -1,10 +1,11 @@
 import NumberFormatOptions = Intl.NumberFormatOptions;
 import NumberFormatPart = Intl.NumberFormatPart;
 import NumberFormatPartTypes = Intl.NumberFormatPartTypes;
-import {NamedMatch, NamedRegExp, replace} from "../characters";
+import {characters, NamedMatch, NamedRegExp, replace} from "../characters";
 import {filter, flatMap, map} from "../transducers";
 import {sequence} from "../sequence";
 import {array} from "../collections";
+import {unique} from "../arrays";
 
 /**
  * Parsing flow
@@ -19,14 +20,14 @@ import {array} from "../collections";
 
 export interface Money {
     currency: string;
-    value: number;
+    amount: number;
 }
 
 export function money(currency: string, amount: number): Money;
 export function money(parts: NumberFormatPart[]): Money;
-export function money(currencyOrParts: string | NumberFormatPart[], value?: number): Money {
-    if (!Array.isArray(currencyOrParts) && value) {
-        return moneyLiteral(currencyOrParts, value);
+export function money(currencyOrParts: string | NumberFormatPart[], amount?: number): Money {
+    if (!Array.isArray(currencyOrParts) && typeof amount === 'number') {
+        return moneyLiteral(currencyOrParts, amount);
     }
     if (Array.isArray(currencyOrParts)) {
         return moneyFrom(currencyOrParts);
@@ -34,8 +35,8 @@ export function money(currencyOrParts: string | NumberFormatPart[], value?: numb
     throw new Error("Invalid arguments");
 }
 
-function moneyLiteral(currency: string, value: number) {
-    return {value, currency};
+function moneyLiteral(currency: string, amount: number): Money {
+    return {amount, currency};
 }
 
 export function moneyFrom(parts: NumberFormatPart[]): Money {
@@ -50,7 +51,7 @@ export type CurrencyDisplay = 'symbol' | 'code';
 
 export function partsFrom(money: Money, locale?: string, currencyDisplay: CurrencyDisplay = 'code'): NumberFormatPart[] {
     const formatter = new Intl.NumberFormat(locale, {currencyDisplay, currency: money.currency, style: 'currency'});
-    return formatter.formatToParts(money.value);
+    return formatter.formatToParts(money.amount);
 }
 
 export function format(money: Money, locale?: string, currencyDisplay: CurrencyDisplay = 'code'): string {
@@ -65,6 +66,16 @@ export function parseToParts(value: string, locale?: string): NumberFormatPart[]
     return regexParser(locale).parse(value);
 }
 
+const currencies = ["AED", "ANG", "AUD", "CHE", "CHF", "CHW", "EUR", "GBP", "HKD", "HNL", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS", "KPW", "KRW", "KWD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "UYI", "UYU", "UYW", "UZS", "VES", "VND", "VUV", "WST", "XAG", "XAU", "XBA", "XBB", "XBC", "XBD", "XCD", "XDR", "XOF", "XPD", "XPF", "XPT", "XSU", "XTS", "XUA", "XXX", "YER", "ZAR", "ZMW", "ZWL"];
+const symbols = currencies.map(symbolFor);
+const symbolPattern = `[${unique(array(sequence(symbols, filter(Boolean), flatMap(characters)))).sort().join('')}]{1,3}`;
+
+export function symbolFor(isoCurrency:string): string | undefined {
+    const parts = partsFrom(money(isoCurrency, 0), undefined, "symbol");
+    const [currency] = parts.filter(p => p.type === 'currency');
+    if (!currency) throw new Error("No currency found");
+    return currency.value !== isoCurrency ? currency.value : undefined;
+}
 
 const exampleMoney = money('GBP', 1234567.89);
 
@@ -72,12 +83,13 @@ export function regexParser(locale?: string): Parser<NumberFormatPart[]> {
     const parts = partsFrom(exampleMoney, locale);
     const [group = ''] = parts.filter(p => p.type === 'group').map(p => p.value);
     const noGroups = parts.filter(p => p.type !== 'group');
+
     let integerAdded = false;
 
     const namedPattern = noGroups.map(part => {
         switch (part.type) {
             case "currency":
-                return '(?<currency>[a-zA-Z]{3})';
+                return `(?<currency>[A-Z]{3}|${symbolPattern})`;
             case "decimal":
                 return `(?<decimal>[${part.value}]?)`;
             case "fraction":
@@ -87,7 +99,7 @@ export function regexParser(locale?: string): Parser<NumberFormatPart[]> {
                     return '';
                 } else {
                     integerAdded = true;
-                    return `(?<integergroup>[0-9${group}]+)`;
+                    return `(?<integergroup>[\\d${group}]+)`;
                 }
             default:
                 return `(?<${part.type}>[${part.value}]?)`;
--- test/money/parsing.test.ts
+++ test/money/parsing.test.ts
@@ -4,8 +4,8 @@ import NumberFormatPart = Intl.NumberFormatPart;
 import {locales} from "../dates/dates.test";
 
 export const numberLocales = Intl.NumberFormat.supportedLocalesOf(locales);
-const currencies = ["AED","ANG","AUD","CHE","CHF","CHW","EUR","GBP","HKD","HNL","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KPW","KRW","KWD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MXV","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","USN","UYI","UYU","UYW","UZS","VES","VND","VUV","WST","XAG","XAU","XBA","XBB","XBC","XBD","XCD","XDR","XOF","XPD","XPF","XPT","XSU","XTS","XUA","XXX","YER","ZAR","ZMW","ZWL"];
-const broken  = ['IQD', 'IRR', 'ISK', 'JPY', 'KPW', 'KRW', 'LAK', 'LBP', 'MGA', 'MMK', 'PYG', 'RSD', 'RWF', 'SLL', 'SOS', 'SYP', 'UGX', 'UYI', 'VND', 'VUV', 'XOF', 'XPF', 'YER'];
+const currencies = ["AED", "ANG", "AUD", "CHE", "CHF", "CHW", "EUR", "GBP", "HKD", "HNL", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS", "KPW", "KRW", "KWD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "UYI", "UYU", "UYW", "UZS", "VES", "VND", "VUV", "WST", "XAG", "XAU", "XBA", "XBB", "XBC", "XBD", "XCD", "XDR", "XOF", "XPD", "XPF", "XPT", "XSU", "XTS", "XUA", "XXX", "YER", "ZAR", "ZMW", "ZWL"];
+const broken = ['IQD', 'IRR', 'ISK', 'JPY', 'KPW', 'KRW', 'LAK', 'LBP', 'MGA', 'MMK', 'PYG', 'RSD', 'RWF', 'SLL', 'SOS', 'SYP', 'UGX', 'UYI', 'VND', 'VUV', 'XOF', 'XPF', 'YER'];
 const amounts = [1234567.89, 156, 156.89, .12];
 
 describe("Money", function () {
@@ -14,13 +14,24 @@ describe("Money", function () {
     const groupDelimetersFound = [',', '.', ' ', '’', ' '];
     // Contains narrow non breaking space - 8239
 
-    it.skip('can parse money', () => {
+    it('can parse money with currency symbol', () => {
+        const m = money('GBP', 123.45);
+        const locale = 'en-GB';
+        const f = format(m, locale);
+        const s = format(m, locale, "symbol");
+        console.log(locale, f, s);
+        const parsed = parse(s, locale);
+        assert.deepEqual(parsed, money('£', 123.45));
+    });
+
+    it('can parse money', () => {
         for (const locale of numberLocales) {
             for (const currency of currencies.filter(c => !broken.includes(c))) {
                 for (const amount of amounts) {
                     const m = money(currency, amount);
                     const f = format(m, locale);
                     const s = format(m, locale, "symbol");
+                    console.log(locale, f, s)
                     const parsed = parse(f, locale);
                     assert.deepEqual(parsed, m);
                 }
