306ebc5492fe96d5c9a349c7722e65f02c82a848
Daniel Worthington-Bodart <dan@bodar.com>
2020-03-05 15:52:53 +0000

    ensure we never accept nearly valid dates

--- src/dates/core.ts
+++ src/dates/core.ts
@@ -11,7 +11,11 @@ declare global {
 export function date(year: number, month?: number, day?: number): Date {
     if (month && (month < 1 || month > 12)) throw new Error(`Invalid month ${month}`);
     if (day && (day < 1 || day > 31)) throw new Error(`Invalid day ${day}`);
-    return new Date(Date.UTC(year, month ? month - 1 : 0, day ? day : 1));
+    const date = new Date(Date.UTC(year, month ? month - 1 : 0, day ? day : 1));
+    if(year !== date.getUTCFullYear()) throw new Error(`Invalid year ${year}`);
+    if(month && month !== (date.getUTCMonth() + 1)) throw new Error(`Invalid month ${month}`);
+    if(day && day !== date.getUTCDate()) throw new Error(`Invalid day ${day}`);
+    return date;
 }
 
 export type MonthFormat = 'numeric' | '2-digit' | 'short' | 'long';
--- test/dates/core.test.ts
+++ test/dates/core.test.ts
@@ -11,4 +11,14 @@ describe("core", function () {
         assert.equal(date(2000).toISOString(), '2000-01-01T00:00:00.000Z');
         assert.equal(date(2000, 2).toISOString(), '2000-02-01T00:00:00.000Z');
     });
+
+    it('does not accept date that is nearly correct', function () {
+        assert.doesNotThrow(() => date(2021, 1, 31));
+        assert.doesNotThrow(() => date(2021, 2, 28));
+        assert.throw(() => date(2021, 2, 29));
+        assert.doesNotThrow(() => date(2020, 2, 29));
+        assert.throw(() => date(2020, 2, 30));
+        assert.doesNotThrow(() => date(2020, 4, 30));
+        assert.throw(() => date(2020, 4, 31));
+    });
 });
