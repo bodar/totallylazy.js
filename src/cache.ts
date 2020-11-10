6f5dc0531d156d13e04d07ecaa35c41720f3597e
Daniel Worthington-Bodart <dan@bodar.com>
2020-11-10 11:38:16 +0000

    caching: support functions that return undefined

--- src/cache.ts
+++ src/cache.ts
@@ -1,27 +1,34 @@
 export function cache(target: any, name: string, descriptor: PropertyDescriptor) {
     if (typeof descriptor.value != 'function') throw new Error("@cache can only decorate methods");
 
-    const cache: { [key: string]: any } = {};
+    const cache = new Map<string, any>();
 
     return Object.defineProperty(target, name, {
         ...descriptor,
         value: function (...args: any[]) {
             const key = JSON.stringify(args);
-            // @ts-ignore
-            const result = cache[key];
+
+            const result = cache.get(key);
             if (typeof result !== 'undefined') return result;
-            return cache[key] = descriptor.value.call(this, ...args);
+            if(cache.has(key)) return result;
+            const value = descriptor.value.call(this, ...args);
+            cache.set(key, value);
+            return value;
         }
     });
 }
 
 export function caching<F extends Function>(fun: F): F {
-    const cache: { [key: string]: any } = {};
+    const cache = new Map<string, any>();
 
     return function (...args: any[]) {
         const key = JSON.stringify(args);
-        const result = cache[key];
+
+        const result = cache.get(key);
         if (typeof result !== 'undefined') return result;
-        return cache[key] = fun(...args);
+        if(cache.has(key)) return result;
+        const value = fun(...args);
+        cache.set(key, value);
+        return value;
     } as any;
 }
--- test/cache.test.ts
+++ test/cache.test.ts
@@ -1,5 +1,66 @@
 import { assert } from 'chai';
-import {caching} from "../src/cache";
+import {cache, caching} from "../src/cache";
+
+describe('cache', () => {
+    it('only calls the underlying function once per value', () => {
+        let count = 0;
+
+        class Foo {
+            @cache add(a: number, b: number) {
+                count++;
+                return a + b;
+            }
+        }
+
+        const foo = new Foo();
+
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,3);
+        assert.equal(count, 2);
+    });
+
+    it('also works if the function returns something falsy', () => {
+        let count = 0;
+        class Foo {
+            @cache add(a: number, b: number) {
+                count++;
+                return false;
+            }
+        }
+
+        const foo = new Foo();
+
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,3);
+        assert.equal(count, 2);
+    });
+
+    it('also works if the function returns undefined', () => {
+        let count = 0;
+        class Foo {
+            @cache add(a: number, b: number) {
+                count++;
+                return undefined;
+            }
+        }
+
+        const foo = new Foo();
+
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,2);
+        assert.equal(count, 1);
+        foo.add(1,3);
+        assert.equal(count, 2);
+    });
+});
+
 
 describe('caching', () => {
     it('only calls the underlying function once per value', () => {
@@ -22,6 +83,21 @@ describe('caching', () => {
         const cachingFN = caching((a:number, b:number) => {
             count++;
             return false;
+        });
+
+        cachingFN(1,2);
+        assert.equal(count, 1);
+        cachingFN(1,2);
+        assert.equal(count, 1);
+        cachingFN(1,3);
+        assert.equal(count, 2);
+    });
+
+    it('also works if the function returns undefined', () => {
+        let count = 0;
+        const cachingFN = caching((a:number, b:number) => {
+            count++;
+            return undefined;
         });
 
         cachingFN(1,2);
