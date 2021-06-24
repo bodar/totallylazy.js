afcb8122b81e31bbf050c5460a69cd396a87cec9
Daniel Worthington-Bodart <dan@bodar.com>
2021-06-24 16:39:04 +0100

    guard against horrible libraries like mootools that break Array.from

--- src/arrays.ts
+++ src/arrays.ts
@@ -2,10 +2,14 @@ export function flatten<T>(values: T[][]): T[] {
     return values.reduce((a, ms) => a.concat(ms), []);
 }
 
-const setSupported = typeof Set === 'function' && new Set([1]).size === 1;
+function isNativeFunction(instance: any): boolean {
+    return typeof instance === 'function' && instance.toString().includes('native code');
+}
+
+const setSupported = isNativeFunction(Set) && new Set([1]).size === 1;
 
 export function unique<T>(a: T[]): T[] {
-    if (typeof Array.from === 'function' && setSupported) return Array.from(new Set(a));
+    if (isNativeFunction(Array.from) && setSupported) return Array.from(new Set(a));
 
     const result = [];
     for (const item of a) {
