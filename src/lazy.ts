84eb074e4a6a817b6f4ac5bff4a0e2d05aebace9
Server <server@bodar.com>
2023-11-16 23:01:05 +0000

    Fix lazy bug

--- src/lazy.ts
+++ src/lazy.ts
@@ -2,24 +2,14 @@ export function replace<T extends {}, K extends keyof T>(object: T, key: K, valu
     return Object.defineProperty(object, key, {value});
 }
 
-export function lazy(target: any, name: string, descriptor: PropertyDescriptor) {
-    if (typeof descriptor.get == 'undefined') throw new Error("@lazy can only decorate getter methods");
-    return Object.defineProperty(target, name, {
+export function lazy(_target: any, name: string, descriptor: PropertyDescriptor) {
+    if (typeof descriptor.get === 'undefined') throw new Error("@lazy can only decorate getter methods");
+    return {
         ...descriptor,
         get(): any {
-            // @ts-ignore
-            return replace(this, name, descriptor.get.call(this))[name]
+            const result = descriptor.get!.call(this);
+            replace(this as any, name, result);
+            return result;
         }
-    });
+    };
 }
-
-export function container<C>(activators: Activators<C>): C {
-    return Object.keys(activators).reduce((container, activator) =>
-        lazy(container, activator, {
-            configurable: true,
-            get: () => (activators as any)[activator](container)
-        }), <C>{});
-}
-
-export type Activator<C, T> = (c: C) => T;
-export type Activators<C> = { [P in keyof C]: Activator<C, C[P]> }
